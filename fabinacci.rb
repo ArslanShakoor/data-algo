63105832eada69ce1efda7778b820879417d73eb
Arslan Shakoor <arslantechielive@gmail.com>
2017-08-26 05:39:25 -0400

    fabinacci

--- fabinacci.rb
+++ fabinacci.rb
@@ -23,23 +23,61 @@
 #   end   
 # end
 
-def fabinacci(num)
-	fabarray = []
-  if num <= 1
-  	return num
-  else
-  	x = 1
-  	fabarray[0] = 0
-  	fabarray[1] = 1
+# def fabinacci(num)
+# 	fabarray = []
+#   if num <= 1
+#   	return num
+#   else
+#   	x = 1
+#   	fabarray[0] = 0
+#   	fabarray[1] = 1
 
-  	while(x < num) do
-  		 x += 1
-      fabarray[x] = fabarray[x - 1] + fabarray[x - 2]
-  	end 
-    return fabarray[num]  	
-  end	
+#   	while(x < num) do
+#   		 x += 1
+#       fabarray[x] = fabarray[x - 1] + fabarray[x - 2]
+#   	end 
+#     return fabarray[num]  	
+#   end	
+# end
+# p fabinacci(0)
+# p fabinacci(2)
+# p fabinacci(5)
+# p fabinacci(100)
+
+def fabinacci(num)
+  if num <= 1
+   return num
+  else
+    num = fabinacci(num - 1) + fabinacci(num - 2)
+  end
 end
+
+def fabinacciIterate(num)
+  if num <= 1
+    return num
+  else
+    x = 1
+    first = 0
+    second = 1
+    while (x < num) do
+      temp = second
+      second = first + second
+      first = temp
+      x += 1
+    end
+  end
+  second
+end
+
+
+
+
 p fabinacci(0)
-p fabinacci(2)
+p fabinacci(1)
 p fabinacci(5)
-p fabinacci(100)
+p fabinacci(20)
+
+p fabinacciIterate(0)
+p fabinacciIterate(1)
+p fabinacciIterate(5)
+p fabinacciIterate(20)  
--- reverse_string.rb
+++ reverse_string.rb
@@ -1,22 +1,54 @@
+# # def array_reversed(string)
+# #   str = string.split("") 
+# #   traversed = []
+# #   str.length.times { traversed << str.pop }
+# #   traversed.join
+# # end
+
 # def array_reversed(string)
-#   str = string.split("") 
-#   traversed = []
-#   str.length.times { traversed << str.pop }
-#   traversed.join
-# end
+# 	puts string
 
-def array_reversed(string)
-	puts string
+#   string.length < 2 ? string : string[-1] + array_reversed(string[1..-2]) + string[0]
 
-  string.length < 2 ? string : string[-1] + array_reversed(string[1..-2]) + string[0]
-
-end	
+# end	
 
 
 
-p array_reversed('')
-p array_reversed('a')
-p array_reversed('reverse')
-p array_reversed('let me reverse it')	
+# p array_reversed('')
+# p array_reversed('a')
+# p array_reversed('reverse')
+# p array_reversed('let me reverse it')	
+
+def reverse(string)
+  str = string.split("")
+  resultarray = []
+  str.length.times do
+    resultarray << str.pop  
+  end
+  resultarray.join()
+end
+
+def reverseInPlace(string)
+  string.length < 2 ? string : string[-1] +  reverseInPlace(string[1..-2]) + string[0]
+end
+
+def reverseInIteration(string)
+  x = 0
+  while(x < string.length/2) do
+    temp = string[x]
+    string[x] = string[string.length - 1 - x]
+    string[string.length - 1 -x] = temp
+    x += 1
+  end 
+  string
+end  
+
+p reverse("hello")
+p reverse("hello! where are you going")
 
 
+p reverseInPlace("hello")
+p reverseInPlace("hello! where are you going")
+
+p reverseInIteration("hello")
+p reverseInIteration("hello! where are you going")
