f1ba394208640d1452767c54946aa490ba2a69e3
Arslan Shakoor <arslantechielive@gmail.com>
2017-08-28 17:04:56 -0400

    factorial

--- factorial.rb
+++ factorial.rb
@@ -1,27 +1,58 @@
-def factorial(number)
-	if number <= 1
-		return 1
-	else
-		number * factorial(number-1)
-	end	
+# def factorial(number)
+# 	if number <= 1
+# 		return 1
+# 	else
+# 		number * factorial(number-1)
+# 	end	
 
-end	
+# end	
 
-def iterative_factorial(number)
-	i = 1
-	product = 1
-  while (i <= number) do
-    product = i * product
-  	i += 1
-  end	
-  return product
+# def iterative_factorial(number)
+# 	i = 1
+# 	product = 1
+#   while (i <= number) do
+#     product = i * product
+#   	i += 1
+#   end	
+#   return product
+# end
+
+
+# p factorial(5)
+# p factorial(2)
+# p factorial(20)
+
+# p iterative_factorial(5)
+# p iterative_factorial(2)
+# p iterative_factorial(20)
+
+def factorial(num)
+  if num < 2 
+    return 1
+  end  
+  fac = num * factorial(num - 1)
+end
+ 
+def factorial1(num)
+  if num < 2
+    return 1
+  end
+  x = 2
+  factorial = 1
+  while(x <= num) do
+    factorial = factorial * x
+    x += 1
+  end
+  factorial
 end
 
 
-p factorial(5)
-p factorial(2)
-p factorial(20)
 
-p iterative_factorial(5)
-p iterative_factorial(2)
-p iterative_factorial(20)
+
+p factorial(0)
+p factorial(1)
+p factorial(5)
+
+p factorial1(0)
+p factorial1(1)
+p factorial1(5)
